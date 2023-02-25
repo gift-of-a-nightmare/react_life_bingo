43ed4c345d5fbe793a36c7da153e1c1f89c46c7c
Lynn! <11156411+WickedMerryAstral@users.noreply.github.com>
2023-02-25 16:06:23 +0100

    json functionality, woo!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import './App.css';
+import achievements from './achievements.json'
+import React, { useEffect } from 'react';
 
 function App() {
+  const [cheevos, setCheevos] = React.useState([]);
+
   return (
     <div className="App">
-      <div className='bg'>
-        <div className='card'>
+      <div className='body'>
+        <div className="container">
+
+      {achievements.map((obj, i) => (
+        <div key={i}>
+          <ContainerCard
+          title={obj.title}
+          description={obj.description}
+          pic={obj.pic}/>
+        </div>
+      ))}
 
         </div>
       </div>
     </div>
   );
+}
+
+function ContainerCard(props) {
+  return (
+    <div className='container-card'>
+      <div className='container-item1'>
+        <img className='cheevo-pic' src={`${props.pic}`}/>
+
+      </div>
+      <div className='container-item2'>
+        <div className='container-title'>
+          <h1>{props.title}</h1>
+        </div>
+        <div className='container-description'>
+          <h3>{props.description}</h3>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
